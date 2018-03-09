7fea170a35ffad06e078a08d8c8baf57d973aef3
JayTrairat <codeoutpost@gmail.com>
2018-03-09 16:21:51 +0700

    try(): read file with fstream

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
+
 int main(){
-        cout << "Hello world";
+        ifstream fileTarget;
+        fileTarget.open("assets/type_1.txt");
+        if(fileTarget.is_open()){
+            string line;
+            while (!fileTarget.eof()) {
+                getline(fileTarget, line);
+                cout << line << '\n';
+            }
+            fileTarget.close();
+        }
         return 0;
 }
