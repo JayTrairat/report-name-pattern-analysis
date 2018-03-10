bca8550713456ad47596da30d0c3c1037331a22d
JayTrairat <codeoutpost@gmail.com>
2018-03-11 00:00:50 +0700

    feat(): filter stop words

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
+    stop_words = [
+        'Hello',
+    ]
     with open('assets/type_1_original.txt', 'r') as file_object:
         content = file_object.readlines()
+
     with open('assets/type_1_output.txt', 'w') as file_object_output:
-        for element in content:
-            file_object_output.write(element)
+        content = [element.strip().split('|') for element in content]
+        for elements in content:
+            filtered_elements = ([element for element in elements if element not in stop_words])
+            joined_filtered_elements = '|'.join(filtered_elements)
+            file_object_output.write(joined_filtered_elements + '\n')
 
 if __name__ == "__main__":
     main()
