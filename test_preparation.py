68b56ca1d527d8b899ce8aaa82533b698ff82866
JayTrairat <codeoutpost@gmail.com>
2018-03-26 16:07:24 +0700

    feat(): insert data to table

--- test_preparation.py
+++ test_preparation.py
@@ -10,34 +10,33 @@ def main():
     }
     try:
         connection = connector.connect(**config)
+        for index in range(0, 8):
 
-        content = []
-        with open('assets/type_1/output_only_1_export.csv', 'r', encoding='utf8') as file:
-            # read contents each line into array
-            contents = file.readlines()
-            # remove empty line
-            contents = [content.strip() for content in contents]
-            # split content in each line by | seperator
-            contents = [content.split('|') for content in contents]
+            content = []
+            with open(('assets/type_{type}/output_only_{type}_export.csv').format(**dict(type = str(index+1))), 'r', encoding='utf8') as file:
+                contents = file.readlines()
+                contents = [content.strip() for content in contents]
+                contents = [content.split('|') for content in contents]
 
-        cursor = connection.cursor(dictionary=True)
-        cursor.execute('truncate naming_elements_original')
-        connection.commit()
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(("truncate naming_elements_original_type_{type}").format(**dict(type = str(index+1))))
+            connection.commit()
 
-        for content in contents:
-            for item in content:
-                query_1 = ("insert into naming_elements_original(datatype_id, naming_element, naming_position) values('{datatype_id}', '{naming_element}', '{naming_position}')")
-                query_2 = ("insert into naming_elements_tagged(datatype_id, naming_element, naming_position) values('{datatype_id}', '{naming_element}', '{naming_position}')")
-                vars = dict(
-                    datatype_id = '0',
-                    naming_element = item,
-                    naming_position = 'UNKNOWN'
-                )
-                cursor.execute(query_1.format(**vars))
-                cursor.execute(query_2.format(**vars))
-                connection.commit()
+            for content in contents:
+                for item in content:
+                    query_1 = ("insert into naming_elements_original_type_{type}(datatype_id, naming_element, naming_position) values('{datatype_id}', '{naming_element}', '{naming_position}')")
+                    query_2 = ("insert into naming_elements_tagged_type_{type}(datatype_id, naming_element, naming_position) values('{datatype_id}', '{naming_element}', '{naming_position}')")
+                    vars = dict(
+                        type = str(index+1),
+                        datatype_id = '0',
+                        naming_element = item,
+                        naming_position = 'UNKNOWN'
+                    )
+                    cursor.execute(query_1.format(**vars))
+                    cursor.execute(query_2.format(**vars))
+                    connection.commit()
 
-        cursor.close()
+            cursor.close()
 
     except connector.Error as error:
         print(error)
