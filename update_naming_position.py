d298c0e9ebb02dcfab08450915deb911566ef775
JayTrairat <codeoutpost@gmail.com>
2018-03-27 15:10:28 +0700

    try(): add naming element but not working

--- update_naming_position.py
+++ update_naming_position.py
@@ -10,14 +10,21 @@ def main():
         connection = connector.connect(**dbconfig['mysql'])
         cursor = connection.cursor(dictionary=True, buffered=True)
 
+
+        PRE = []
+        MIDPRE = []
+        MIDPOST = []
+        POST = []
+
+
         cursor.execute("select id, naming_element from naming_elements_tagged_type_1")
-        cursor_for_update = connection.cursor(dictionary=True, buffered=True)
+        # cursor_for_update = connection.cursor(dictionary=True, buffered=True)
         for elem in (cursor):
-            query = "update naming_elements_tagged_type_1 set naming_position = 'PRE' where id = '{id}'".format(**dict(id = elem['id']))
-            print(query)
-            cursor_for_update.execute(query)
-        cursor_for_update.close()
-        connection.commit()
+            # query = "update naming_elements_tagged_type_1 set naming_position = 'PRE' where id = '{id}'".format(**dict(id = elem['id']))
+            print(elem['naming_element'])
+            # cursor_for_update.execute(query)
+        # cursor_for_update.close()
+        # connection.commit()
         cursor.close()
     except Exception as e:
         print(e)
