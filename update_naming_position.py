c89b52b6e6fdd63378f906005157d664bf781274
JayTrairat <codeoutpost@gmail.com>
2018-03-27 14:30:46 +0700

    feat(): update position with script

--- update_naming_position.py
+++ update_naming_position.py
@@ -1,8 +1,28 @@
 import mysql.connector as connector
 from string import Template
+import yaml
 
 def main():
-    print('hello world')
+    with open('dbconfig.yml', 'r') as config:
+        dbconfig = yaml.load(config)
+
+    try:
+        connection = connector.connect(**dbconfig['mysql'])
+        cursor = connection.cursor(dictionary=True, buffered=True)
+
+        cursor.execute("select id, naming_element from naming_elements_tagged_type_1")
+        cursor_for_update = connection.cursor(dictionary=True, buffered=True)
+        for elem in (cursor):
+            query = "update naming_elements_tagged_type_1 set naming_position = 'PRE' where id = '{id}'".format(**dict(id = elem['id']))
+            print(query)
+            cursor_for_update.execute(query)
+        cursor_for_update.close()
+        connection.commit()
+        cursor.close()
+    except Exception as e:
+        print(e)
+    else:
+        connection.close()
 
 if __name__ == "__main__":
     main()
