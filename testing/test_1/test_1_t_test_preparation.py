e3c55d93243cebcfdd99086cadd3bca1df0ad76e
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-11 15:59:14 +0700

    refactor(): remove sorting function

--- testing/test_1/test_1_t_test_preparation.py
+++ testing/test_1/test_1_t_test_preparation.py
@@ -2,7 +2,7 @@ def main():
     with open('assets/type_1/cosine_values_only_value.txt', 'r', encoding='utf8') as source:
         original_contents = source.readlines()
         original_contents = [content.strip() for content in original_contents]
-        original_contents.sort(reverse=True)
+        # original_contents.sort(reverse=True)
         original_contents = original_contents
         with open('assets/type_1/cosine_values_for_ttest.txt', 'w', encoding='utf8') as result:
             result.write('\n'.join(original_contents))
@@ -10,7 +10,7 @@ def main():
     with open('assets/type_1_refactor_naming_elements/cosine_values_only_value.txt', 'r', encoding='utf8') as source:
         contents = source.readlines()
         contents = [content.strip() for content in contents]
-        contents.sort(reverse=True)
+        # contents.sort(reverse=True)
         contents = contents
         with open('assets/type_1_refactor_naming_elements/cosine_values_for_ttest.txt', 'w', encoding='utf8') as result:
             result.write('\n'.join(contents))
